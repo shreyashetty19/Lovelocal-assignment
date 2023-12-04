b9f9db43091d61b7ea50da99964801e837475833
shreya shetty <116868004+shreyashetty19@users.noreply.github.com>
2023-12-04 16:17:42 +0530

    load

---
+++ easy1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        
+        // Remove leading and trailing whitespaces from the input string
+        String mystr = s.trim();
+        String[] str = mystr.split(" ");
+        
+         // Get the last substring from the array
+        String laststring = str[str.length - 1];
+        System.out.println(laststring.length());
+    }
+}
